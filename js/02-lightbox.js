5035eeb0d0b1da14e38aa9a076524455428a9188
Ridrox93 <popovandrii@yahoo.com>
2023-08-29 19:07:21 +0200

    andrii

---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+    `;
+    })
+    .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    
+  });
